7318f8318adf852904892271f966d43a2f9e0691
Hannah <hannah.greenwell@gmail.com>
2018-09-24 20:51:38 +1000

    Added Functionality to Check for a Win

--- js/main.js
+++ js/main.js
@@ -1,17 +1,13 @@
+
 const t3 = {
+
   rows: [
-    ['', '', ''],
-    ['', '', ''],
-    ['', '', ''],
+    [1, 2, 3],
+    [4, 1, 6],
+    [7, 8, 1],
   ],
 
-  printRows: function(){
-    for(let i = 0; i < this.rows.length; i++){
-      for(let j = 0; j < this.rows[i].length; j++){
-        console.log(this.rows[i][j]);
-      } // inner for
-    } // outer for
-  }, // printRows
+  width: 2, // Change to variable
 
   addMove: function(xPos, yPos, player){
     const selectedCell = this.rows[xPos][yPos];
@@ -25,4 +21,70 @@ const t3 = {
       return;
     }
   }, // addMove
+
+
+  // Takes an array of 3 cells and checks to see if all three are equal. Returns true if all three are equal and false if all three are not equal.
+  // CHECK FOR ERRORS
+  checkAllEqual: function(cells){
+    const firstCell = cells[0];
+    const allEqual = cells.every(i => i === firstCell);
+    return allEqual;
+  }, // checkAllEqual
+
+  checkRow: function(xPos){
+    const row = this.rows[xPos];
+    return this.checkAllEqual(row);
+  },
+
+  checkColumn: function(yPos){
+    const column = [];
+
+    for(let i = 0; i < this.rows.length; i++){
+      column[i] = this.rows[i][yPos];
+    }
+
+    return this.checkAllEqual(column);
+  },
+
+  checkDiagonal: function(xPos, yPos){
+    let allEqual = false;
+
+    if(xPos === yPos){
+      const diagonal = [];
+
+      for(let i = 0; i < this.rows.length; i++){
+        diagonal[i] = this.rows[i][i];
+      }
+      allEqual = this.checkAllEqual(diagonal);
+    }
+
+    if((xPos + yPos) === this.width){
+      const diagonal = [];
+
+      for(let i = 0; i < this.rows.length; i++){
+        diagonal[i] = this.rows[i][this.width - i];
+      }
+      allEqual = this.checkAllEqual(diagonal); // Error: Changes previous true to false.
+    }
+
+    return allEqual;
+  },
+
+  checkForWin: function(xPos, yPos){
+    if(this.checkRow(xPos) === true){
+      console.log('You won! (Row)');
+      return true;
+    }
+    if(this.checkColumn(yPos)){
+      console.log('You won! (Column)');
+      return true;
+    }
+    if(this.checkDiagonal(xPos, yPos)){
+      console.log('You won! (Diagonal)');
+      return true;
+    }
+
+    return false;
+  }, // checkForWin
+
 }; // ticTacToe
